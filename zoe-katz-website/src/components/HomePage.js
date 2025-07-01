fc10be58788b1988092239644e3ef4c7d00d6767
zoekatz14 <zoek65272@gmail.com>
2025-07-01 17:19:00 -0500

    home page

---
+++ zoe-katz-website/src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <header>
+        <h1>About Me</h1>
+      </header>
+      <main>
+        <p>This is my bio.</p>
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
