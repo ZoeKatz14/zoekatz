ea8043e5dd8665dc932e2af571d49a1a672eb47e
zoekatz14 <zoek65272@gmail.com>
2025-07-02 11:45:03 -0500

    added footer

---
+++ zoe-katz-website/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css';
+
+
+const Footer=()=>{
+        return (  
+                <div className="footer">
+                        <p className='email-link'>
+                                Contact me: <a href="mailto:zoek65272@gmail.com">zoek65272@gmail.com</a>
+                        </p>
+                        <p>
+                                Site last updated: 01.01.2000
+                        </p>
+                </div>
+        )
+    
+}
+export default Footer;    
