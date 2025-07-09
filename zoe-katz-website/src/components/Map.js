6fc6bf14ad696c9bff57226ebeb999824a75a60f
zoekatz14 <zoek65272@gmail.com>
2025-07-09 09:33:25 -0500

    working map w/o book data

--- zoe-katz-website/src/components/Map.js
+++ zoe-katz-website/src/components/Map.js
@@ -1,15 +1,77 @@
-import React from 'react';
+import React, {memo} from 'react';
+import 'react-tooltip/dist/react-tooltip.css'
 import './Map.css';
-import { ReactComponent as WorldMap} from '../world.svg';
+import { ComposableMap, Geographies, Geography, ZoomableGroup } from "react-simple-maps";
+const geoUrl =
+  "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json";
 
+// To be filled with my book data at a later point
+// const data = [
+//         {
+//           name: "Australia",
+//           book: "ABC"
+//         }
+// ]
 
+const mapWidth = 800;
+const mapHeight = 420;
 
-const Map=()=>{
+const Map=({setTooltipContent})=>{
         return (  
-                <div className='map-content'>
-                <WorldMap />
+                <div className='map-content' data-tip="">
+                <ComposableMap
+                width={mapWidth}
+                height={mapHeight}
+                projectionConfig={{ scale: 160 }}
+                >
+                <ZoomableGroup 
+                center={[0, 10]} 
+                zoom={1.5} 
+                minZoom={1.5} maxZoom={10}
+                translateExtent={[
+                        [0, 0],
+                        [mapWidth, mapHeight]
+                        ]}
+                >
+                        <Geographies geography={geoUrl}>
+                                {({ geographies }) =>
+                                geographies.map((geo) => (
+
+                                <Geography 
+                                key={geo.rsmKey} 
+                                geography={geo} 
+                                data-tooltip-id="my-tooltip"
+                                onMouseEnter={() => {
+                                        setTooltipContent(`${geo.properties.name}`);
+                                }}
+                                onMouseLeave={() => {
+                                        setTooltipContent("");
+                                }}
+                                style={{
+                                        default: {
+                                          fill: "#8896AB",
+                                          outline: "#353535",
+                                          stroke: "#C5D5E4",
+                                          strokeWidth: 0.5
+                                        },
+                                        hover: {
+                                          fill: "#0EA5E9",
+                                          outline: "#353535"
+                                        },
+                                        pressed: {
+                                          fill: "#E42",
+                                          outline: "#353535"
+                                        }
+                                      }}
+                                />
+
+                                ))
+                                }
+                        </Geographies>
+                </ZoomableGroup>
+                </ComposableMap>
                 </div>
         )
     
 }
-export default Map;    
+export default memo(Map);    
--- zoe-katz-website/src/pages/FunPage.js
+++ zoe-katz-website/src/pages/FunPage.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Pages.css';
 import Map from '../components/Map.js';
+import {Tooltip} from 'react-tooltip';
 
 const FunPage = () => {
+  const [content, setContent] = useState("");
   return (
     <div className='page'>
       <header>
         <h1>reading around the world</h1>
       </header>
       <main>
-        <Map />
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-      </main>
+        <div>
+          <Map setTooltipContent={setContent}/>
+          <Tooltip id="my-tooltip">{content}</Tooltip>
+          {/* To show up on the page under the map as well: {content} */}
+        </div>
+       <p> You can pan and zoom on the map above! To pan, drag your mouse. To zoom, scroll up or down. 
+          <br></br>When you hover over a country, you can see its name. </p>      
+       </main>
     </div>
   );
 }
